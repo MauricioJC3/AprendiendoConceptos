8b768e55bfd7f2b2b5d422d99587e7994b074556
Andres Mauricio Jimenez Chavez <142183331+MauricioJC3@users.noreply.github.com>
2025-01-21 17:45:27 -0500

    feat(Verificacion 2FA): router

--- Laravel/Laravel_Cero/Laravel-01-Practicas/routes/web.php
+++ Laravel/Laravel_Cero/Laravel-01-Practicas/routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Hola mundo!';
 });
+
+Route::get('/post/{post}', function ($post) {
+    return 'post ' . $post;
+});
+
