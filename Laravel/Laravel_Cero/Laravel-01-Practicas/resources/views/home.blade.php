5bae3e7f0ed1d55172060e19505f54dea89f9890
Andres Mauricio Jimenez Chavez <142183331+MauricioJC3@users.noreply.github.com>
2025-01-22 16:43:45 -0500

    feat(Laravel): segunda clase complements

--- Laravel/Laravel_Cero/Laravel-01-Practicas/resources/views/home.blade.php
+++ Laravel/Laravel_Cero/Laravel-01-Practicas/resources/views/home.blade.php
@@ -5,8 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Inicio</title>
+    <!-- Tailwind CSS -->
+    <script src="https://cdn.tailwindcss.com"></script>
+
 </head>
 <body>
-    <h1>Bienvendio a Laravel!</h1>
+    <div class="max-w-4xl mx-auto px-4">
+        {{-- <h1>Bienvendio a Laravel!</h1> --}}
+
+        {{-- alerta dinamica --}}
+        <x-alert type="danger">
+            <x-slot name="title">
+                No se encuantran posts!
+            </x-slot>
+            contenido de la alerta
+        </x-alert>
+
+    </div>
 </body>
 </html>
